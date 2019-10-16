fb800dd022ed3aba46566a7b44c69ef3702de985
Jean Carlo Ovalles <jeanov92@gmail.com>
2019-10-16 11:14:41 -0500

    first commit

---
+++ src/main/java/com/firebase/FirebaseBasicApplication.java
@@ -0,0 +1,13 @@
+package com.firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseBasicApplication.class, args);
+	}
+
+}
